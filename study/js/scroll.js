4d44e9693dd3b1371910ded22f96d3fc4f5adb82
jds6408 <64389087+jds6408@users.noreply.github.com>
2020-07-20 19:23:08 +0900

    study

--- study/js/scroll.js
+++ study/js/scroll.js
@@ -34,7 +34,3 @@ window.addEventListener('scroll', function () {
     }
     
 });
-
-/*window.addEventListener('scroll', function() {
-  document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
-});*/
