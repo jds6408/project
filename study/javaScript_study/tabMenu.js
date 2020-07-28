47edadbe09ff84d20d4a308156bcbe7f4dfc65ab
jds6408 <64389087+jds6408@users.noreply.github.com>
2020-07-28 13:15:15 +0900

    tabMenu

--- study/javaScript_study/tabMenu.js
+++ study/javaScript_study/tabMenu.js
@@ -1,33 +1,22 @@
 const tabList = document.querySelectorAll('.tab_list');
-//foreach 는 Array의 메서드
-//querySelectorAll 은 nodeList객체 반환(배열과비슷하지만 배열은 아님)
-//call()메서드를 통해 tabList == this로 설정
-//주어진 this값 및 각각 전달된 인수와 함께 함수 호출
+const tabContent = document.querySelectorAll('.tab_content');
+const targetLink = document.querySelectorAll('.tab_list a')
+tabList.forEach(function (index) {
+    for (let i = 0; i < tabList.length; i++) {
+        tabList[i].addEventListener('click', function (e) {
+            const tabNum = tabList[i].getAttribute('data-tabnum');
+            // console.log(tabNum);
+            e.preventDefault();
+            for (let j = 0; j < tabContent.length; j++) {
+                tabContent[j].style.display = "none";
+            }
+            document.getElementById('content' + (i + 1)).style.display = "block";
 
+            for (let k = 0; k < targetLink.length; k++) {
+                targetLink[k].classList.remove('active');
+                e.target.classList.add('active');
+            }
 
-Array.prototype.forEach.call(tabList, function (list) {
-    //console.log(list); // list== tablist[div]
-
-    //a를 클릭시
-    list.children[0].addEventListener('click', function (e) {
-        e.preventDefault(); //눌렀을 때 a의 특성 페이지 이동하는 거 막아줌
-        const tabContent = document.querySelectorAll('.tab_container .tab_content');
-        const tabNum = this.parentElement.getAttribute('data-tabnum');
-        // console.log(tabNum);
-        // console.log(e.target);
-        console.log(e.target);
-        if(e.target){
-            e.target.parentElement.classList.toggle('active');
-        }
-
-        Array.prototype.forEach.call(tabContent, function (cont, i) {
-            //array.forEach(callbackFunction(currnetValue, index, array), thisArg)
-            //console.log(cont, i);
-            //tabContent, tabnum
-            cont.style.display = "none";
         })
-        //위 foreach문 빠져나왔음.
-        tabContent[tabNum].style.display = "block";
-    })
-
-})
+    }
+});
