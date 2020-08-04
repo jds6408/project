2f0e6f8ef75595108a9db2961299f12a2d8bc3e3
jds6408 <64389087+jds6408@users.noreply.github.com>
2020-08-04 21:37:25 +0900

    animation-js

--- study/javaScript_study/js/animation.js
+++ study/javaScript_study/js/animation.js
@@ -1,2 +1,32 @@
 'use strict';
+let imgObj = null;
+const btn = document.querySelector('input');
 
+function init() {
+    imgObj = document.querySelector('img');
+    imgObj.style.position = "relative";
+    imgObj.style.left = '0px';
+}
+let play;
+
+function playAni() {
+    play = setInterval(function () {
+        imgObj.style.left = parseInt(imgObj.style.left) + 100 + "px";
+    }, 2000);
+}
+
+function stopAni() {
+    clearInterval(play);
+}
+btn.addEventListener("click", function () {
+
+    if (btn.value === "play") {
+        playAni();
+        btn.value = 'stop';
+    } else {
+        stopAni();
+        btn.value = 'play';
+    }
+})
+
+init();
